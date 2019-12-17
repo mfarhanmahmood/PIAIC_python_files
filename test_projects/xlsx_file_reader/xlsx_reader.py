9104eb7871d3f5c1b8eb177974ce943e4ffb37bf
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-12-17 16:25:15 +0500

    Created excel file reader

---
+++ test_projects/xlsx_file_reader/xlsx_reader.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+
+dirname = os.path.dirname(__file__)
+relative_path = '.\\db\\PIAIC Islamabad Batch 3 Quarter 1 Final Grades_Results - Onsite.xlsx'
+file_errors_location = os.path.join(dirname, relative_path)
+df = pd.read_excel(file_errors_location)
+roll_no = input("Enter your roll no. (PIAIC00000): ")
+print(df.loc[df['Roll Number'] == 'PIAIC56326'])

6110e3703324592b2aca0f43021dd440d284bcf2
Muhammad Farhan Mahmood <farhanmahmood433@gmail.com>
2019-12-18 11:19:15 +0500

    Removed error that only check a single roll no.

--- test_projects/xlsx_file_reader/xlsx_reader.py
+++ test_projects/xlsx_file_reader/xlsx_reader.py
@@ -6,4 +6,4 @@ relative_path = '.\\db\\PIAIC Islamabad Batch 3 Quarter 1 Final Grades_Results -
 file_errors_location = os.path.join(dirname, relative_path)
 df = pd.read_excel(file_errors_location)
 roll_no = input("Enter your roll no. (PIAIC00000): ")
-print(df.loc[df['Roll Number'] == 'PIAIC56326'])
+print(df.loc[df['Roll Number'] == roll_no.upper()])
